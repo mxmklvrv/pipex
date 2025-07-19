9ae2a11ce1ae356b3c9760c0bf37ce066c1e4e40
Maxim Kleverov <maximkleverov@icloud.com>
2025-07-19 15:52:52 +0300

    update

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 14:13:33 by mklevero          #+#    #+#             */
-/*   Updated: 2025/07/18 20:17:39 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/07/19 15:48:24 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,39 @@
 # include <unistd.h>
 
 // error msg
-# define ERROR_ARG "Error: Incorrect number of arguments."
-# define ERROR_PIPE "Error: pipe() failed."
+# define ERR_ARG "Error: Incorrect number of arguments."
+# define ERR_PIPE "Error: Pipe() failed."
+# define ERR_FORK_1 "Error: Fork one failed."
+# define ERR_FORK_2 "Error: Fork two failed."
+
+// pipe in and out
+# define WRITE_TO 1
+# define READ_FROM 0
 
 typedef struct s_struct
 {
 	int		ac;
 	char	**av;
-
+    char    *envp;
+    int     pipe_fd[2];
+    pid_t   pid_one;
+    pid_t   pid_two;
+    int     infile_fd;
+    int     outfile_fd;
+    
+    
+    
 }			t_struct;
 
 // main
 int			main(int ac, char **av, char **envp);
+
+// init
+void init_struct(t_struct *data,int ac, char **av, char **envp);
+
+// chiled processing
+void process_child_one(t_struct *data);
+void process_child_two(t_struct *data);
 
 // exit 
 void		exit_error(char *msg, t_struct *data);
